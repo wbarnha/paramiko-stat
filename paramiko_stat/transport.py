347bbe7185d8e02ea8e66568c4f4cdfa52eb24dc
William Barnhart <william.barnhart@he360.com>
2023-04-13 13:57:32 -0400

    Update transport.py

--- paramiko_stat/transport.py
+++ paramiko_stat/transport.py
@@ -1,3 +1,22 @@
+# Copyright (C) 2003-2007  Robey Pointer <robeypointer@gmail.com>
+#
+# This file is part of Paramiko.
+#
+# Paramiko is free software; you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 2.1 of the License, or (at your option)
+# any later version.
+#
+# Paramiko is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Paramiko; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.
+
+
 from paramiko.transport import Transport as _Transport
 
 from .sftp_client import SFTPClient
